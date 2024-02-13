4a3ddb7ba7e562b5c75bab4c8ee6a7808af721ce
Himanshu Mandwal <himanshu.mandwal@idfcfirstbank.com>
2024-02-13 17:02:46 +0530

    added coverage threshold

--- frontend/src/components/Auth/SignIn.test.js
+++ frontend/src/components/Auth/SignIn.test.js
@@ -2,11 +2,11 @@ import { fireEvent, render, screen } from "@testing-library/react";
 import SignIn from "./SignIn";
 
 const mockHistoryPush = jest.fn();
-jest.mock('react-router', () => ({
-    ...jest.requireActual('react-router'),
-    useHistory: () => ({
-      push: mockHistoryPush,
-    }),
+jest.mock("react-router", () => ({
+  ...jest.requireActual("react-router"),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
 }));
 
 test("should have sign in page", () => {
@@ -15,65 +15,64 @@ test("should have sign in page", () => {
   expect(linkElement).toBeInTheDocument();
 });
 
-test("should render username input",() => {
-    render(<SignIn />);
-    const linkElement = screen.getByTestId("username-input");
-    expect(linkElement).toBeInTheDocument();
+test("should render username input", () => {
+  render(<SignIn />);
+  const linkElement = screen.getByTestId("username-input");
+  expect(linkElement).toBeInTheDocument();
 });
 
-test("should able to enter username input",() => {
-    render(<SignIn />);
-    const userNameInput = screen.getByTestId("username-input");
-    fireEvent.change(userNameInput, { target: { value: 'tushar' } });
-    expect(userNameInput).toHaveValue('tushar');
+test("should able to enter username input", () => {
+  render(<SignIn />);
+  const userNameInput = screen.getByTestId("username-input");
+  fireEvent.change(userNameInput, { target: { value: "tushar" } });
+  expect(userNameInput).toHaveValue("tushar");
 });
 
-test("should render password input",() => {
-    render(<SignIn />);
-    const passwordInput = screen.getByTestId("password-input");
-    fireEvent.change(passwordInput, { target: { value: 'tushar' } });
-    expect(passwordInput).toHaveValue('tushar');
-    expect(passwordInput).toBeInTheDocument();
+test("should render password input", () => {
+  render(<SignIn />);
+  const passwordInput = screen.getByTestId("password-input");
+  fireEvent.change(passwordInput, { target: { value: "tushar" } });
+  expect(passwordInput).toHaveValue("tushar");
+  expect(passwordInput).toBeInTheDocument();
 });
 
-test("should able to enter password input",() => {
-    render(<SignIn />);
-    const passwordInput = screen.getByTestId("password-input");
-    fireEvent.change(passwordInput, { target: { value: 'tushar' } });
-    expect(passwordInput).toHaveValue('tushar');
+test("should able to enter password input", () => {
+  render(<SignIn />);
+  const passwordInput = screen.getByTestId("password-input");
+  fireEvent.change(passwordInput, { target: { value: "tushar" } });
+  expect(passwordInput).toHaveValue("tushar");
 });
 
-test("should render submit btn input",() => {
-    render(<SignIn />);
-    const linkElement = screen.getByTestId("submit-btn");
-    expect(linkElement).toBeInTheDocument();
+test("should render submit btn input", () => {
+  render(<SignIn />);
+  const linkElement = screen.getByTestId("submit-btn");
+  expect(linkElement).toBeInTheDocument();
 });
 
-test('should disable the submit button when username and password is not entered', () => {
-    render(<SignIn />);
-    const submitButton = screen.getByTestId("submit-btn");
-    expect(submitButton).toBeDisabled();
+test("should disable the submit button when username and password is not entered", () => {
+  render(<SignIn />);
+  const submitButton = screen.getByTestId("submit-btn");
+  expect(submitButton).toBeDisabled();
 });
 
-test('should enabled the submit button when username and password is entered', () => {
-    render(<SignIn />);
-    const userNameInput = screen.getByTestId("username-input");
-    fireEvent.change(userNameInput, { target: { value: 'tushar' } });
-    const passwordInput = screen.getByTestId("password-input");
-    fireEvent.change(passwordInput, { target: { value: 'tushar' } });
-    const submitButton = screen.getByTestId("submit-btn");
-    expect(submitButton).toBeEnabled();
+test("should enabled the submit button when username and password is entered", () => {
+  render(<SignIn />);
+  const userNameInput = screen.getByTestId("username-input");
+  fireEvent.change(userNameInput, { target: { value: "tushar" } });
+  const passwordInput = screen.getByTestId("password-input");
+  fireEvent.change(passwordInput, { target: { value: "tushar" } });
+  const submitButton = screen.getByTestId("submit-btn");
+  expect(submitButton).toBeEnabled();
 });
 
-
-test('should call handleSubmit when username and password submitted', () => {
-    render(<SignIn />);
-    const userNameInput = screen.getByTestId("username-input");
-    fireEvent.change(userNameInput, { target: { value: 'tushar' } });
-    const passwordInput = screen.getByTestId("password-input");
-    fireEvent.change(passwordInput, { target: { value: 'tushar' } });
-    const submitButton = screen.getByTestId("submit-btn");
-    fireEvent.click(submitButton);
-    expect(userNameInput).toHaveValue('');
-    expect(passwordInput).toHaveValue('');
+test("should call handleSubmit when username and password submitted", () => {
+  render(<SignIn />);
+  const userNameInput = screen.getByTestId("username-input");
+  fireEvent.change(userNameInput, { target: { value: "tushar" } });
+  const passwordInput = screen.getByTestId("password-input");
+  fireEvent.change(passwordInput, { target: { value: "tushar" } });
+  const submitButton = screen.getByTestId("submit-btn");
+  fireEvent.click(submitButton);
+  expect(userNameInput).toHaveValue("");
+  expect(passwordInput).toHaveValue("");
 });
