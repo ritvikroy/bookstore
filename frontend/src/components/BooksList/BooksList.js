39f5f454af08026b8e9b1bc2aadbd9aa80854cb7
Himanshu Mandwal <himanshu.mandwal@idfcfirstbank.com>
2024-02-15 15:10:32 +0530

    updated list books api

--- frontend/src/components/BooksList/BooksList.js
+++ frontend/src/components/BooksList/BooksList.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { useHistory } from "react-router";
 import "./BooksList.css";
+import BOOKSTOREIMAGE from "../../assets/bookstoreicon.jpeg";
 
 const BooksList = ({ booksList }) => {
   const history = useHistory();
-  console.log(">>>", booksList);
 
   const handleBuy = (book) => {
     history.push("/orderConfirmation", {
@@ -15,32 +15,61 @@ const BooksList = ({ booksList }) => {
 
   return (
     <div className="book-list-container" data-testid="books-list">
-      <table className="book-table">
-        <thead>
-          <tr>
-            <th>Book Name</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {booksList.map((book) => (
-            <tr key={book.id}>
-              <td>{book.name}</td>
-              <td>&#8377;{book.price}</td>
-              <td>
-                <button
-                  data-testid={`${book.id}`}
-                  type="button"
-                  onClick={() => handleBuy(book)}
-                >
-                  Buy
-                </button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <div>
+        {booksList.map((book) => (
+          <div
+            key={book.id}
+            style={{
+              display: "flex",
+              backgroundColor: "aliceblue",
+              justifyContent: "space-around",
+              margin: "20px",
+              boxShadow: "0 4px 8px 0 grey, 0 6px 20px 0 grey",
+              borderRadius: "20px",
+            }}
+          >
+            <div style={{ padding: "10px" }}>
+              <img
+                style={{ width: "200px", height: "200px" }}
+                src={BOOKSTOREIMAGE}
+                alt="book"
+              />
+            </div>
+            <div
+              style={{
+                width: "50%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "flex-start",
+                justifyContent: "space-around",
+              }}
+            >
+              <div style={{ fontSize: "28px", fontWeight: "bold" }}>
+                {book.name}
+              </div>
+              <div style={{ fontSize: "22px" }}>
+                Price : &#8377;{book.price}
+              </div>
+            </div>
+            <div
+              style={{
+                width: "10%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <button
+                data-testid={`${book.id}`}
+                type="button"
+                onClick={() => handleBuy(book)}
+              >
+                Buy
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
